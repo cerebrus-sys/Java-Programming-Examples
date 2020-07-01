3be67e3b3aa8e54571ea795e2988f024abd989f5
Sanjan <sanjangeet2109s@gmail.com>
2020-07-01 09:02:27 +0530

    Added Hello World

---
+++ src/Hello/World.java
@@ -0,0 +1,12 @@
+/**
+ * Program to print "Hello World!" on the screen without quotes.
+ */
+
+package Hello;
+
+public class World {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
